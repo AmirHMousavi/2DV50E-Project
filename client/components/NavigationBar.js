776c603c54134a898609f08f5cc51a971e4d74e4
AmirHMousavi <ah.mousavi@hotmail.com>
2016-11-20 17:48:22 +0100

    login and logout on client-side

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router';
-export default() => {
-    return (
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <Link to="/" className="navbar-brand">myApp</Link>
-                </div>
+import {connect} from 'react-redux';
+import {logout} from '../actions/authActions';
 
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul className="nav navbar-nav navbar-right">
-                        <li>
-                            <Link to="/signup">Sign up</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+class NavigationBar extends Component {
+logout(event){
+    event.preventDefault();
+    this.props.logout();
 }
+
+    render() {
+        const {isAuthenticated} = this.props.auth;
+        const userLinks = (
+            <ul className="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#" onClick={this.logout.bind(this)}>LogOut</a>
+                </li>
+            </ul>
+        );
+        const gustLinks = (
+            <ul className="nav navbar-nav navbar-right">
+                <li>
+                    <Link to="/signup">Sign up</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+            </ul>
+        );
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <Link to="/" className="navbar-brand">myApp</Link>
+                    </div>
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    {isAuthenticated ? userLinks : gustLinks}</div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+NavigationBar.propTypes = {
+    auth: React.PropTypes.object.isRequired,
+    logout:React.PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+    return {auth: state.auth};
+}
+
+export default connect(mapStateToProps,{logout})(NavigationBar);
